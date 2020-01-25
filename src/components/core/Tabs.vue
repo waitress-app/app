e803ec57a54c8155fa7fdfa0620a6fad7c9b871a
Renato66 <revi66@hotmail.com>
2020-01-25 13:59:10 -0300

    tabs component wip

---
+++ src/components/core/Tabs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="c-tabs" :style="{'width': `${items.length * 100}px`}">
+    <div class="c-tabs__item"
+      :class="{'c-tabs__item--active': item === value}"
+      @click="$emit('input', item)"
+      v-for="item in items"
+      :key="item">
+      {{ item }}
+    </div>
+    <div class="c-tabs__slider" :style="{'transform': `translateX(${items.findIndex(elem => elem === value) * 100}px)`}"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    },
+    items: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.c-tabs
+  display flex
+  align-items center
+  position relative
+  height 45px
+  margin auto
+  &__item
+    text-align center
+    flex 1
+    color #8790a9
+    transition color .3s cubic-bezier(.25,.8,.5,1)
+    cursor pointer
+    &--active
+      color #8b68f1
+      cursor default
+  &__slider
+    position absolute
+    left 0
+    bottom 0
+    width 100px
+    height 4px
+    background linear-gradient(135deg, #bc4cf6, #7873ef)
+    border-radius 2px
+    transition .3s cubic-bezier(.25,.8,.5,1)
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,59 @@
 <template>
-  <div>
-    oi
+  <div class="c-home">
+    <div class="c-navbar">
+      <div class="c-navbar__avatar">
+        <CAvatar :src="user.picture" size="64" />
+      </div>
+      <div class="c-navbar__name pl-4">
+        <h2 class="title my-0">
+          {{ user.name }}
+        </h2>
+        <p class="c-link my-0">
+          sair
+        </p>
+      </div>
+      <div class="c-navbar__tips">
+        <CTips :goal="100" :tips="54.32" />
+      </div>
+    </div>
+    <CTabs v-model="tab" :items="tabs"/>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import CAvatar from '@/components/core/Avatar'
+import CTips from '@/components/Tips'
+import CTabs from '@/components/core/Tabs'
 export default {
-
+  components: {
+    CAvatar,
+    CTips,
+    CTabs
+  },
+  data () {
+    return {
+      tab: 'Mesas',
+      tabs: ['Mesas', 'Pedidos', 'Menu']
+    }
+  },
+  computed: {
+    ...mapGetters('auth', ['user'])
+  }
 }
 </script>
 
-<style>
+<style lang="stylus">
+// .c-home
+.c-navbar
+  display flex
+  align-items center
+  &__avatar
+    flex-shrink 1
+  &__name
+    flex 1
+  &__tips
+    flex-shrink 1
+    text-align right
 
 </style>
