fdd4eb4506f318a1c0835276f4f56175d56c95cc
Renato66 <revi66@hotmail.com>
2020-01-31 02:17:14 -0300

    icons + pwa

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    name: 'Waitress',
+    themeColor: '#8d68f1',
+    msTileColor: '#FFFFFF',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png'
+      // maskIcon: 'img/icons/safari-pinned-tab.svg',
+      // msTileImage: 'img/icons/ms-icon-144x144.png'
+    }
+  }
+}
