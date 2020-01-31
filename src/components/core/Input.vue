b50722a6cc30e1a9d4e86d6a517086c0f46869bc
Renato66 <revi66@hotmail.com>
2020-01-31 01:33:37 -0300

    new core components

---
+++ src/components/core/Input.vue
@@ -0,0 +1,42 @@
+<template>
+  <input class="c-input-text" :class="{'c-input-text--block': block}" type="text" v-model="inputVal">
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    },
+    block: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    inputVal: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.c-input-text
+  padding 10px 20px
+  border 1px solid #cccccc
+  border-radius 25px
+  font-size 18px
+  height 24px
+  transition all .3s ease
+  outline none
+  &:focus
+    border-color #8d68f1
+  &--block
+    width calc(100% - 42px)
+</style>
---
+++ src/components/core/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <transition name="fade">
+    <div class="c-modal" v-show="value">
+      <div class="c-modal__backdrop" @click="$emit('input', false)"></div>
+      <div class="c-modal__paper">
+        <slot />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.c-modal
+  position absolute
+  top 0
+  left 0
+  bottom 0
+  right 0
+  z-index 99
+  display flex
+  align-items center
+  &__paper
+    margin auto
+    padding 10px
+    background white
+    border-radius 8px
+  &__backdrop
+    position absolute
+    top 0
+    left 0
+    bottom 0
+    right 0
+    z-index -1
+    background #04040429
+</style>
