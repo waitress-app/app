64676f9ebd97f318cb22cc0273f3646f078ab9fa
Renato Vicente <revi66@hotmail.com>
2020-04-14 02:01:29 -0300

    wip orders item

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -3,8 +3,13 @@
     <div class="headline" v-if="!orders">
       Sem pedidos
     </div>
-    <div v-for="order in orders" :key="order.id">
-      {{ order }}
+    <div v-for="order in orders" :key="order.id" class="c-orders--item">
+      <div>
+        {{ order.quantity }} - {{ order.item.text }}
+      </div>
+      <div>
+        para: {{ order.customers.map(elem => elem.name).join(', ') }}
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +25,9 @@ export default {
 
 <style lang="stylus">
 .c-orders
-  text-align center
   color #cdcad6
+  &--item
+    border-bottom 1px dashed gray
+    margin-bottom 12px
+    padding-bottom 12px
 </style>
